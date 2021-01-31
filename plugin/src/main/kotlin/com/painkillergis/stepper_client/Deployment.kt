8b171e197df99879492a66d67e0dbd56997127aa
tyler <tyler@cruftbusters.com>
2021-01-30 20:51:52 -0600

    print deployment information (testing would solve this)

--- plugin/src/main/kotlin/com/painkillergis/stepper_client/Deployment.kt
+++ plugin/src/main/kotlin/com/painkillergis/stepper_client/Deployment.kt
@@ -4,14 +4,19 @@ import io.ktor.client.features.*
 import io.ktor.client.request.*
 import kotlinx.coroutines.delay
 
-suspend fun deploy(serviceName: String, group : String, imageName: String, version: String) {
+suspend fun deploy(serviceName: String, group: String, imageName: String, version: String) {
+  val deployment = mapOf(
+    "group" to group,
+    "imageName" to imageName,
+    "version" to version,
+  )
+
+  println("sending deployment request:")
+  println(deployment)
+
   newStepperClient().post<Unit>("/services/$serviceName/deployment") {
     header("content-type", "application/json")
-    body = mapOf(
-      "group" to group,
-      "imageName" to imageName,
-      "version" to version,
-    )
+    body = deployment
   }
 
   val stepperDarkClient = newClient("painkiller.arctair.com", serviceName)
